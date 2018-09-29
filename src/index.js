b9bc20107656be156a05a287a70b8382bac5a589
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-09-29 02:06:29 -0400

    Added react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './views/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={App} />
+            {/* <Route path="/test" component={App} />
+            <Route component={NotFound} /> */}
+        </Switch>
+    </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
