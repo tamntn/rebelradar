245baddd7d979cf0f05db20289eeafab853c28a5
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-09-29 05:08:22 -0400

    Update Card

--- src/views/App.js
+++ src/views/App.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
-import { Input } from 'antd';
+import { Icon, Input } from 'antd';
 import './App.css';
+import BarCard from './BarCard';
+import appLogo from '../image/logo.png';
+
+const Search = Input.Search;
 
 class App extends Component {
-  render() {
-    return (
-      <Input placeholder="Basic usage" />
-    );
-  }
+	render() {
+		return (
+			<div className="app">
+				<div className="header">
+					<div className="title">
+						<img src={appLogo} />
+						Rebel Radar
+        		</div>
+				</div>
+				<div className="body">
+					<div className="main-actions">
+						<Search
+							placeholder="Search..."
+							size="large"
+						/>
+					</div>
+					<div className="cards-collection">
+						<BarCard />
+						<BarCard />
+						<BarCard />
+						<BarCard />
+						<BarCard />
+						<BarCard />
+						<BarCard />
+						<BarCard />
+						<BarCard />
+						<BarCard />
+					</div>
+				</div>
+				<div className="footer">
+					Rebel Radar © - VolHacks 2018
+        		</div>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/views/BarCard.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Card, Icon, Progress, Skeleton } from 'antd';
+import './BarCard.css';
+
+const strokeColor = {
+    "1": "#ffccc7",
+    "2": "#ffa39e",
+    "3": "#ff7875",
+    "4": "#ff4d4f",
+    "5": "#f5222d",
+}
+
+class BarCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            rating: 15
+        }
+        this.getRatingStrokeColor = this.getRatingStrokeColor.bind(this);
+    }
+
+    getRatingStrokeColor(rating) {
+        if (rating >= 90)
+            return strokeColor["5"]
+        else if (rating >= 70)
+            return strokeColor["4"]
+        else if (rating >= 50)
+            return strokeColor["3"]
+        else if (rating >= 20)
+            return strokeColor["2"]
+        else
+            return strokeColor["1"]
+    }
+
+    render() {
+        const rating = this.state.rating;
+        const ratingStrokeColor = this.getRatingStrokeColor(rating);
+
+        return (
+            <Card
+                title="BAR NAME 🔥"
+                hoverable
+                style={{ width: 280 }}
+                cover={<img alt="example" src="https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/15871466_1644719459162000_8163189671313138506_n.jpg?_nc_cat=104&oh=60b48f0ea8088615771e71dbae376e40&oe=5C5B5408" />}
+            // actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
+            >
+                <Skeleton loading={this.state.loading}>
+                    <div className="card-content">
+                        <div className="price">No Cover</div>
+                        <Progress percent={rating} status="active" showInfo={false} strokeWidth={7} strokeColor={ratingStrokeColor} />
+                        <div className="update-time">Updated 7 minutes ago</div>
+                    </div>
+                </Skeleton>
+            </Card>
+        )
+    }
+}
+
+export default BarCard;
