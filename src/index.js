eccbc7a6d4a3347f9c93e93480d0175ca610070f
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-09-30 05:08:32 -0400

    Add https redirect component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HttpsRedirect from 'react-https-redirect';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './index.css';
 import App from './views/App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 import InfoDrawer from './views/InfoDrawer';
+import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/drawer" component={InfoDrawer} />
-            {/* <Route component={NotFound} /> */}
-        </Switch>
-    </BrowserRouter>, document.getElementById('root'));
+    <HttpsRedirect>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/drawer" component={InfoDrawer} />
+                {/* <Route component={NotFound} /> */}
+            </Switch>
+        </BrowserRouter>
+    </HttpsRedirect>, document.getElementById('root'));
 registerServiceWorker();
